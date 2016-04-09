411afc618a85e01ec3de39c376e33dba7c63f9b1
martarzy <marta.bandarzy@gmail.com>
2016-04-09 17:59:44 +0200

    Field coordinates

--- client/Field.ts
+++ client/Field.ts
@@ -2,15 +2,17 @@
     export class Field {
         // statyczna tablica ze wspolrzednymi kolejnych pol -> tymczasowo
         // @TODO: uzupelnic tablice wartosciami
+        // duze - 63
+        // male - 50
         private static FieldCoords = [
-            // pola na dole (od lewej)
-            [], [], [], [], [], [], [], [], [], [],
+            // pola na dole (od prawej)
+            [580, 585], [517, 585], [465, 585], [415, 585], [365, 585], [315, 585], [265, 585], [215, 585], [165, 585], [115, 585], [52, 585],
             // pola po lewej (od dolu)
-            [], [], [], [], [], [], [], [], [], [],
-            // pola u gory (od prawej)
-            [], [], [], [], [], [], [], [], [], [],
+            [52, 522], [52, 472], [52, 422], [52, 372], [52, 322], [52, 272], [52, 222], [52, 172], [52, 122],
+            // pola u gory (od lewej)
+            [52, 59], [115, 59], [165, 59], [215, 59], [265, 59], [315, 59], [365, 59], [415, 59], [465, 59], [517, 59], [580, 59],
             // pola po prawej (od gory)
-            [], [], [], [], [], [], [], [], [], []
+            [580, 122], [580, 172], [580, 222], [580, 272], [580, 322], [580, 372], [580, 422], [580, 472], [580, 522]
         ];
         private fieldId: number;
         // wspolrzedne srodkow pol
