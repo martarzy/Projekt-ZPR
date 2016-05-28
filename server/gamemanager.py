d078191886d11172492852945e3e597290fc2a5b
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2016-05-28 19:00:26 +0200

    Added field buying support

--- server/gamemanager.py
+++ server/gamemanager.py
@@ -7,7 +7,15 @@ class Player:
         self.handler = handler  # Handler is a class which must have write_message() function
         self.ready = False
         self.cash = 1500
-        self.field = 0
+        self.field_nr = 0
+
+
+class Field:
+    def __init__(self, price):
+        self.owner = None
+        self.price = price
+        self.buyable = price > 0
+        self.bought = False
 
 
 class GameManager:
@@ -16,6 +24,12 @@ class GameManager:
         self.turn = -1
         self.pname_map = {}
         self.started = False
+        field_prices = [0, 60, 0, 60, 0, 200, 100, 0, 100, 120,
+                        0, 140, 150, 140, 160, 200, 180, 0, 180, 200,
+                        0, 220, 0, 220, 240, 200, 260, 260, 150, 280,
+                        0, 300, 300, 0, 320, 200, 0, 350, 0, 400]
+
+        self.fields = [Field(price) for price in field_prices]
 
     def add_player(self, name, handler):
         new_player = Player(name, handler)
@@ -44,11 +58,14 @@ class GameManager:
             result = self.roll_dice()
             msg = dict(message='playerMove', player=player.name, move=result)
             self.broadcast(msg)
-            player.field += result
-            if player.field >= 40:
+            player.field_nr += result
+            if player.field_nr >= 40:
                 player.cash += 400
-                player.field -= 40
+                player.field_nr -= 40
                 self.broadcast_cash_info(player)
+
+        elif msg['message'] == 'buyField':
+            self.buy_field(player)
 
         elif msg['message'] == 'endOfTurn':
             self.next_turn()
@@ -90,6 +107,15 @@ class GameManager:
         self.turn = (self.turn + 1) % len(self.players)
         msg = {'message': 'newTurn', 'player': self.players[self.turn].name}
         self.broadcast(msg)
+
+    def buy_field(self, player):
+        field = self.fields[player.field_no]
+        if field.buyable and not field.bought:
+            field.bought = True
+            self.broadcast_field_buy(player)
+
+    def broadcast_field_buy(self, player):
+        self.broadcast({'message': 'userBought', 'username': player.name})
 
     def broadcast_cash_info(self, player):
         self.broadcast({'message': 'setCash', 'cash': player.cash, 'player': player.name})
