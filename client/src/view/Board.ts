b120ebdbec33228cf12c25e9e7c3e5322a051d24
Julian Rubin <rubin94@gmail.com>
2016-05-30 00:24:56 +0200

    Pawn traverses valid distance in view

--- client/src/view/Board.ts
+++ client/src/view/Board.ts
@@ -25,12 +25,13 @@ namespace view {
 
         public movePawn(pawnName: string, fieldNumber: number, onMovingEnd: () => any) {
             let sequencenumber = 0;
-            for (let i = (this.pawns[pawnName].getPawnField().getFieldId()) % 40;
+            for (let i = (this.pawns[pawnName].getPawnField().getFieldId() + 1) % 40;
                 i != fieldNumber;
                 i = (i + 1) % 40)
             {
                 this.pawns[pawnName].move(this.fields[i], sequencenumber++);
             }
+            this.pawns[pawnName].move(this.fields[fieldNumber], sequencenumber);
             setTimeout(onMovingEnd, sequencenumber*200);
         }
     }
