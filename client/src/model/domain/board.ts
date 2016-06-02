c49f95a2bd6b49d9d14482f008aa569bf382b098
Julian Rubin <rubin94@gmail.com>
2016-06-02 13:09:11 +0200

    Fields initialization changed

--- client/src/model/domain/board.ts
+++ client/src/model/domain/board.ts
@@ -10,52 +10,6 @@ namespace model {
             this.initializeFields();
         }
 
-        private initializeFields(): void {
-            this.fields_.push(new Field(this.fields_.length, "Go"));
-            this.fields_.push(new Field(this.fields_.length, "Brown", 60, 50));
-            this.fields_.push(new Field(this.fields_.length, "Chance"));
-            this.fields_.push(new Field(this.fields_.length, "Brown", 60, 50));
-            this.fields_.push(new Field(this.fields_.length, "Income Tax"));
-            this.fields_.push(new Field(this.fields_.length, "Railroad", 200));
-            this.fields_.push(new Field(this.fields_.length, "Blue", 100, 50));
-            this.fields_.push(new Field(this.fields_.length, "Chance"));
-            this.fields_.push(new Field(this.fields_.length, "Blue", 100, 50));
-            this.fields_.push(new Field(this.fields_.length, "Blue", 120, 50));
-
-            this.fields_.push(new Field(this.fields_.length, "Jail"));
-            this.fields_.push(new Field(this.fields_.length, "Pink", 140, 100));
-            this.fields_.push(new Field(this.fields_.length, "Utility", 150));
-            this.fields_.push(new Field(this.fields_.length, "Pink", 140, 100));
-            this.fields_.push(new Field(this.fields_.length, "Pink", 160, 100));
-            this.fields_.push(new Field(this.fields_.length, "Railroad", 200));
-            this.fields_.push(new Field(this.fields_.length, "Orange", 160, 100));
-            this.fields_.push(new Field(this.fields_.length, "Chance"));
-            this.fields_.push(new Field(this.fields_.length, "Orange", 160, 100));
-            this.fields_.push(new Field(this.fields_.length, "Orange", 180, 100));
-
-            this.fields_.push(new Field(this.fields_.length, "Parking"));
-            this.fields_.push(new Field(this.fields_.length, "Red", 220, 150));
-            this.fields_.push(new Field(this.fields_.length, "Chance"));
-            this.fields_.push(new Field(this.fields_.length, "Red", 220, 150));
-            this.fields_.push(new Field(this.fields_.length, "Red", 240, 150));
-            this.fields_.push(new Field(this.fields_.length, "Railroad", 200));
-            this.fields_.push(new Field(this.fields_.length, "Yellow", 260, 150));
-            this.fields_.push(new Field(this.fields_.length, "Yellow", 260, 150));
-            this.fields_.push(new Field(this.fields_.length, "Utility", 150));
-            this.fields_.push(new Field(this.fields_.length, "Yellow", 280, 150));
-
-            this.fields_.push(new Field(this.fields_.length, "Go to jail"));
-            this.fields_.push(new Field(this.fields_.length, "Green", 300, 200));
-            this.fields_.push(new Field(this.fields_.length, "Green", 300, 200));
-            this.fields_.push(new Field(this.fields_.length, "Chance"));
-            this.fields_.push(new Field(this.fields_.length, "Green", 320, 200));
-            this.fields_.push(new Field(this.fields_.length, "Railroad", 200));
-            this.fields_.push(new Field(this.fields_.length, "Chance"));
-            this.fields_.push(new Field(this.fields_.length, "Navy", 350, 200));
-            this.fields_.push(new Field(this.fields_.length, "Luxury tax"));
-            this.fields_.push(new Field(this.fields_.length, "Navy", 350, 200));
-        }
-
         getField(id: number): Field {
             return this.fields_[id];
         }
@@ -75,6 +29,51 @@ namespace model {
 
         fields(): Array<Field> {
             return this.fields_;
+        }
+
+        private initializeFields(): void {
+            this.fields_ = [
+                new Field(0, "Go"),
+                new Field(1, "Brown", 60, 50),
+                new Field(2, "Chance"),
+                new Field(3, "Brown", 60, 50),
+                new Field(4, "Income Tax"),
+                new Field(5, "Railroad", 200),
+                new Field(6, "Blue", 100, 50),
+                new Field(7, "Chance"),
+                new Field(8, "Blue", 100, 50),
+                new Field(9, "Blue", 120, 50),
+                new Field(10, "Jail"),
+                new Field(11, "Pink", 140, 100),
+                new Field(12, "Utility", 150),
+                new Field(13, "Pink", 140, 100),
+                new Field(14, "Pink", 160, 100),
+                new Field(15, "Railroad", 200),
+                new Field(16, "Orange", 160, 100),
+                new Field(17, "Chance"),
+                new Field(18, "Orange", 160, 100),
+                new Field(19, "Orange", 180, 100),
+                new Field(20, "Parking"),
+                new Field(21, "Red", 220, 150),
+                new Field(22, "Chance"),
+                new Field(23, "Red", 220, 150),
+                new Field(24, "Red", 240, 150),
+                new Field(25, "Railroad", 200),
+                new Field(26, "Yellow", 260, 150),
+                new Field(27, "Yellow", 260, 150),
+                new Field(28, "Utility", 150),
+                new Field(29, "Yellow", 280, 150),
+                new Field(30, "Go to jail"),
+                new Field(31, "Green", 300, 200),
+                new Field(32, "Green", 300, 200),
+                new Field(33, "Chance"),
+                new Field(34, "Green", 320, 200),
+                new Field(35, "Railroad", 200),
+                new Field(36, "Chance"),
+                new Field(37, "Navy", 350, 200),
+                new Field(38, "Luxury tax"),
+                new Field(39, "Navy", 350, 200)
+            ];
         }
     }
 
