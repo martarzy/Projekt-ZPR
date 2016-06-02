47fb1738164d3e800938a921e964b421eaca9903
Julian Rubin <rubin94@gmail.com>
2016-06-02 04:03:56 +0200

    Partitioned method refactored

--- client/src/model/utils.ts
+++ client/src/model/utils.ts
@@ -2,12 +2,7 @@
 
     export function partition<T>(array: Array<T>, predicate: (arg: T) => boolean): [Array<T>, Array<T>] {
         const partitioned: [Array<T>, Array<T>] = [[], []];
-        array.forEach(elem => {
-            if (predicate(elem))
-                partitioned[0].push(elem);
-            else
-                partitioned[1].push(elem);
-        });
+        array.forEach(elem => partitioned[predicate(elem) ? 0 : 1].push(elem));
         return partitioned;
     }
     
