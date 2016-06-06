d8fd60e2c8cc1ac366a87f48027f6dd80e227163
martarzy <marta.bandarzy@gmail.com>
2016-06-06 15:14:05 +0200

    Empty fields - trade

--- client/src/view/view.ts
+++ client/src/view/view.ts
@@ -170,6 +170,8 @@ namespace view {
             $("#offered-menu").children().remove();
             for (var i = 0; i < list.length; i++)
                 $("#offered-menu").append('<li><a href="#">' + list[i] + '</a></li>');
+            // dodatkowe puste pole
+            $("#offered-menu").append('<li><a href="#"></a></li>');
 
             this.selectValueFromDropdownMenu("offered-menu");
         }
@@ -178,6 +180,7 @@ namespace view {
             $("#requested-menu").children().remove();
             for (var i = 0; i < list.length; i++)
                 $("#requested-menu").append('<li><a href="#">' + list[i] + '</a></li>');
+            $("#requested-menu").append('<li><a href="#"></a></li>');
 
             this.selectValueFromDropdownMenu("requested-menu");
         }
